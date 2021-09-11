48de6ef81b00fd0b81b3179949336704ddb1c7ea
nhan nguyen <dev@nhan.io>
2021-09-11 08:21:52 +0700

    Initial Commit

---
+++ ringSwiftUI/ringSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ringSwiftUIApp.swift
+//  ringSwiftUI
+//
+//  Created by dak on 11/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct ringSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
